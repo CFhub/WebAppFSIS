9b01bcdab82bdf3a1525dbfc5e4c44a430ec7d1f
Clarisse Fourie <cfourie1@nait.ca>
2020-03-09 13:17:18 -0600

    a

---
+++ FSISSystem/ENTITIES/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FSISSystem.ENTITIES
+{
+    [Table("Player")]
+    public class Player
+    {
+        public int PlayerID { get; set; }
+        public string RegionDescription { get; set; }
+    }
+}
